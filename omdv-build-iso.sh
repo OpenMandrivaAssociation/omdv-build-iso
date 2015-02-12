ea73b5e5ee64667fd022de32cfb8905e16ef68f4
tpg (Tomasz Paweł Gajc) <tpgxyz@gmail.com>
2015-02-13 00:13:47 +0100

    add temp workaround fo abf

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -219,6 +219,10 @@ updateSystem() {
     #Force update of critical packages
 	if [ "$ABF" = "1" ]; then
 	    echo "We are inside ABF (www.abf.io)"
+	    # workaround
+	    $SUDO urpmi.removemedia -a
+	    $SUDO urpmi.addmedia --distrib --mirrorlist http://abf.rosalinux.ru/downloads/cooker/repository/x86_64
+	    # end of workaround
 	    $SUDO urpmq --list-url
 	    $SUDO urpmi.update -ff updates
     # inside ABF, lxc-container which is used to run this script is based
