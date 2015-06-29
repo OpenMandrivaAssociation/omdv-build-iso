640852710b9897a85de32ae78ca6e26fb53aa0d2
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2015-06-29 10:52:07 +0400

    Clean the rpm db files

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -431,6 +431,9 @@ createChroot() {
 	KERNEL_ISO=`ls -d --sort=time [0-9]* |head -n1 | sed -e 's,/$,,'`
 	export KERNEL_ISO
     popd
+    
+    #remove rpm db files which may not match the target chroot environment
+    $SUDO chroot "$CHROOTNAME" rm -f /var/lib/rpm/__db.*
 
 }
 
@@ -871,6 +874,9 @@ EOF
 #    fi
 
     if [ -n "$NOCLEAN" ]; then
+    
+	#remove rpm db files which may not match the non-chroot environment
+    $SUDO chroot "$CHROOTNAME" rm -f /var/lib/rpm/__db.*
 
     # add urpmi medias inside chroot
     echo "Removing old urpmi repositories."
@@ -891,7 +897,7 @@ EOF
 	else
 	    MIRRORLIST="http://downloads.openmandriva.org/mirrors/$TREE.$EXTARCH.list"
 	fi
-	echo "Using $MIRROLIST"
+	echo "Using $MIRRORLIST"
 	$SUDO urpmi.addmedia --urpmi-root "$CHROOTNAME" --wget --no-md5sum --distrib --mirrorlist $MIRRORLIST
     fi
 
