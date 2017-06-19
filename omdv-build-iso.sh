c135e8614168b4a9bd67e2e01fad563f37d3f7db
itchka (Colin Close) <itchka@compuserve.com>
2017-06-19 07:36:11 +0100

    Revert previous commit. Add a delay instead

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -441,7 +441,7 @@ printf "%s $WORKDIR"
 	# Inside ABF, lxc-container which is used to run this script is based
 	# on Rosa2012 which does not have cdrtools
 	# List of packages that needs to be installed inside lxc-container and local machines
-	RPM_LIST="perl-URPM dosfstools grub2 xorriso syslinux squashfs-tools bc imagemagick kpartx gdisk gptfdisk parallel util-linux"
+	RPM_LIST="perl-URPM dosfstools grub2 xorriso syslinux squashfs-tools bc imagemagick kpartx gdisk gptfdisk parallel"
 
 	printf "%s\n -> Installing rpm files %s\n"
 	$SUDO urpmi --downloader wget --wget-options --auth-no-challenge --auto --no-suggests --verify-rpm --ignorearch ${RPM_LIST} --prefer /distro-theme-OpenMandriva-grub2/ --prefer /distro-release-OpenMandriva/ --auto
@@ -1213,6 +1213,7 @@ createUEFI() {
     $SUDO mkfs.vfat -C -F 16 -s 1 -S 512 -M 0xFF $IMGNME $EFIDISKSIZE
 # Loopback mount the image
     $SUDO losetup -f $IMGNME
+    sleep 2
     if [[ $? != 0 ]]; then
 	printf "%s\n -> Failed to mount loopback image. Exiting."
 	errorCatch
