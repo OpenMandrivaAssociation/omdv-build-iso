1bea2ec474ee4a9d42896d42fc3e94da7dcbfe81
itchka (Colin Close) <itchka@compuserve.com>
2018-09-13 21:39:40 +0100

    re-enable --parallel in this branch

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -1037,8 +1037,13 @@ mkUserSpin() {
 MyAdd() {
 	if [ -n "$__install_list" ]; then 
 		printf "%s\n" "-> Installing user package selection" " "
+	    if [ -n "$PLL" ]; then
+		printf "%s\n" "$__install_list" | parallel -q --keep-order --joblog "$WORKDIR/install.log" --tty --halt now,fail=$MAXERRORS -P 1 /usr/bin/dnf install -y --refresh --nogpgcheck --forcearch=x86_64 --exclude=*.i686 --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log"
+		printf "%s\n" "$__install_list" >"$WORKDIR/RPMLIST.txt"
+	    else
 		printf "%s\n" "$__install_list" | xargs /usr/bin/dnf install -y --refresh --nogpgcheck --forcearch=x86_64 --exclude=*.i686 --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log"
 		printf "%s\n" "$__install_list" >"$WORKDIR/RPMLIST.txt"
+	    fi
 	fi
 }
 
@@ -1094,9 +1099,9 @@ mkUpdateChroot() {
 	elif [ "$IN_ABF" = '1' ]; then
 		#printf "%s\n" "-> Installing packages at ABF"
 		if [ -n "$PLLL" ]; then
-			printf "%s\n" "$__install_list" | xargs /usr/bin/dnf install -y --refresh --forcearch=x86_64 --exclude=*.i686 --nogpgcheck --setopt=install_weak_deps=False --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log"
+			printf "%s\n" "$__install_list" | parallel -q --keep-order --joblog "$WORKDIR/install.log" --tty --halt now,fail="$MAXERRORS" -P 1 /usr/bin/dnf install -y --refresh --forcearch=x86_64 --exclude=*.i686 --nogpgcheck --setopt=install_weak_deps=False --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log"
 		else
-			printf '%s\n' "$__install_list" | xargs /usr/bin/dnf  install -y --refresh  --nogpgcheck --forcearch=x86_64 --exclude=*.i686 --setopt=install_weak_deps=False --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log" 
+			printf '%s\n' "$__install_list" | xargs /usr/bin/dnf install -y --refresh --forcearch=x86_64 --exclude=*.i686 --nogpgcheck --setopt=install_weak_deps=False --installroot "$CHROOTNAME"  | tee "$WORKDIR/dnfopt.log" 
 		fi
 	fi
 }
