772d906777e24e6fde59335b088dbf2a2d4cec95
Gabriel Craciunescu <nix.or.die@gmail.com>
2019-01-18 19:55:06 +0100

    temp set compress* to XZ

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -234,7 +234,8 @@ main() {
 	[ -z "${TREE}" ] && TREE=cooker
 	[ -z "${VERSION}" ] && VERSION="$(date +%Y.0)"
 	[ -z "${RELEASE_ID}" ] && RELEASE_ID=alpha
-	[ -z "${COMPTYPE}" ] && COMPTYPE="zstd -Xcompression-level 15"
+	# (crazy) fixme , --compression= is broken
+	[ -z "${COMPTYPE}" ] && COMPTYPE="xz -Xbcj x86 -Xdict-size 100%"
 	[ -z "${MAXERRORS}" ] && MAXERRORS=1
 	if [[ ( "$IN_ABF" = '1'  &&  -n "$DEBUG" )  ||  "$IN_ABF" = '0' ]]; then
 		if [ -z "$NOCLEAN" ]; then
