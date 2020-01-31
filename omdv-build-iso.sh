bab40db35e9e8426b831e42d60a019b39d5731b3
Bernhard Rosenkraenzer <bero@lindev.ch>
2020-01-31 18:22:44 +0100

    Start packagekitd before running pkcon

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -1185,7 +1185,7 @@ createChroot() {
 	# remove rpm db files which may not match the target chroot environment
 	chroot "$CHROOTNAME" rm -f /var/lib/rpm/__db.*
 	# Cache stuff to make discover happy - but don't freak out on an iso that doesn't include PK
-	chroot "$CHROOTNAME" pkcon refresh force || :
+	chroot "$CHROOTNAME" /bin/bash -c "/usr/libexec/packagekitd & sleep 5s; pkcon refresh force; /usr/libexec/packagekitd --immediate-exit" || :
 }
 
 # Usage: mkOmSpin [main install file path} i.e. [path]/omdv-kde4.lst.
