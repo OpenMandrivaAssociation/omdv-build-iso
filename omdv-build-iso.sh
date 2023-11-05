fef20f7fa839e2cb23618d42edfb063b1b496a59
rugyada <46781309+rugyada@users.noreply.github.com>
2023-11-05 13:59:59 +0100

    clean blank spaces

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -784,7 +784,7 @@ getPkgList() {
 		cd "$WORKDIR" || exit
 		EX_PREF=./
 		EXCLUDE_LIST="--exclude ${EX_PREF}.abf.yml --exclude ${EX_PREF}ChangeLog --exclude ${EX_PREF}Developer_Info --exclude ${EX_PREF}Makefile --exclude ${EX_PREF}README --exclude ${EX_PREF}TODO --exclude ${EX_PREF}omdv-build-iso.sh --exclude ${EX_PREF}omdv-build-iso.spec --exclude ${EX_PREF}docs/*  --exclude ${EX_PREF}tools/* --exclude ${EX_PREF}ancient/*"
-		wget -qO- https://github.com/OpenMandrivaAssociation/omdv-build-iso/archive/"${GIT_BRNCH}".zip | bsdtar -xvf- ${EXCLUDE_LIST} --strip-components 1 
+		wget -qO- https://github.com/OpenMandrivaAssociation/omdv-build-iso/archive/"${GIT_BRNCH}".zip | bsdtar -xvf- ${EXCLUDE_LIST} --strip-components 1
 		if [ ! -e "$FILELISTS" ]; then
 			printf "%s\n" "-> $FILELISTS does not exist. Exiting"
 			errorCatch
@@ -1467,7 +1467,7 @@ createUEFI() {
 		errorCatch
 	fi
 	sleep 1
- 
+
 mount -t vfat "$IMGNME" /mnt
 	if [ $? != 0 ]; then
 		printf "%s\n" "-> Failed to mount UEFI image." "Exiting."
