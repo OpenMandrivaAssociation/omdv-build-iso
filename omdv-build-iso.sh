0acec1d7c9e2393fa20a948e38f735be4e6422f8
Bernhard Rosenkränzer <bero@lindev.ch>
2026-02-10 23:20:20 +0100

    Don't try to install grub gfxterm_menu

--- omdv-build-iso.sh
+++ omdv-build-iso.sh
@@ -1418,7 +1418,7 @@ createMemDisk() {
 	chroot "$CHROOTNAME"  /usr/bin/grub2-mkimage -O "$ARCHFMT" -d "$ARCHLIB" -m memdisk_img -o "/ISO/EFI/BOOT/$EFINAME" -p '(memdisk)/boot/grub' \
 	iso9660 normal memdisk tar linux part_msdos part_gpt part_apple configfile help loadenv ls reboot chain multiboot fat udf \
 	ext2 btrfs ntfs reiserfs xfs lvm ata cat test echo multiboot multiboot2 all_video efifwsetup efinet font gcry_rijndael gcry_rsa gcry_serpent \
-	gcry_sha256 gcry_twofish gcry_whirlpool gfxmenu gfxterm gfxterm_menu gfxterm_background gzio halt hfsplus jpeg mdraid09 mdraid1x minicmd part_apple \
+	gcry_sha256 gcry_twofish gcry_whirlpool gfxmenu gfxterm gfxterm_background gzio halt hfsplus jpeg mdraid09 mdraid1x minicmd part_apple \
 	part_msdos part_gpt part_bsd password_pbkdf2 png probe \
 	search search_fs_uuid search_fs_file search_label sleep tftp video xfs loopback regexp
 
